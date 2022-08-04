1149c58a2ce3fdb4e4bbc5230ad0785f405b64c0
MarcRaeder <marc.raeder@5minds.de>
2022-08-04 12:49:39 +0200

    add mario-more

---
+++ Week1/mario-more/mariomore.c
@@ -0,0 +1,44 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    //user input
+    int n;
+    do
+    {
+        n = get_int("Size: ");
+    }
+    while (n < 1 || n > 8);
+
+    //Pyramide
+    for (int i = 0; i < n; i++)
+    {
+
+        for (int j = 0; j < n - i; j++){
+            printf(" ");
+        }
+
+
+        for (int j = 0; j < i + 1; j++)
+        {
+
+
+            printf("#");
+        }
+
+        printf("  ");
+
+        for (int j = 0; j < i + 1; j++)
+        {
+
+
+            printf("#");
+        }
+
+
+
+
+        printf("\n");
+    }
+}
