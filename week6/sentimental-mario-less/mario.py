dcb98347bf43e11be2832d05bb656b032e3badf5
Marc Räder <marcrader@Marcels-MBP.office.5minds.de>
2022-08-23 12:13:39 +0200

    add sentimental-mario-less

---
+++ week6/sentimental-mario-less/mario.py
@@ -0,0 +1,12 @@
+while True:
+    try:
+        Height = int(input("Height: "))
+    except ValueError:
+        print("Height have to be an integer!")
+    if Height < 9 and Height > 0:
+        break
+    else:
+        print("Height have to be between 1 - 8")
+
+for i in range(Height):
+    print(" " * (Height - i), "#" * (i + 1))
