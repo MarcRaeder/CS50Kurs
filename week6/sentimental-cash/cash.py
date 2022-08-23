a90f5aa89015839912e19a366bf91f8566c18412
Marc Räder <marcrader@Marcels-MBP.office.5minds.de>
2022-08-23 14:18:44 +0200

    add cash.py

---
+++ week6/sentimental-cash/cash.py
@@ -0,0 +1,46 @@
+from shelve import DbfilenameShelf
+
+def main():
+
+    while True:
+        try:
+            cents = float(input("Change owed: ")) * 100
+            if cents > 0:
+                break
+            else:
+                print("cents have to be bigger than 0")
+        except ValueError:
+            print("only use numbers")
+        
+
+    quarters = calculate_quarters(cents)
+    cents -= quarters * 25
+
+    dimes = calculate_dimes(cents)
+    cents -= dimes * 10
+
+    nickles = calculate_nickles(cents)
+    cents -= nickles * 5
+
+    pennies = cents 
+
+    coins = int(quarters + dimes + nickles + pennies)
+
+    print(coins)
+
+def calculate_quarters(cents):
+    quarters = cents // 25
+
+    return quarters
+
+def calculate_dimes(cents):
+    dimes = cents // 10
+
+    return dimes
+
+def calculate_nickles(cents):
+    nickles = cents // 5
+
+    return nickles
+
+main()
