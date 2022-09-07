39cfc9fcf75033316aaf6b33f582696ddd2a65dc
Marc Räder <marcrader@Marcels-MBP.office.5minds.de>
2022-09-07 08:10:15 +0200

    fix requested changes (Codestyle

--- week8/homepage/javascript.js
+++ week8/homepage/javascript.js
@@ -3,8 +3,9 @@ var list = document.getElementById("Animelist");
 function addtext() {
     var Textinput = document.getElementById("Textinput").value;
     var Entry = document.createElement("li");
-    
+
     Entry.appendChild(document.createTextNode(Textinput));
     list.appendChild(Entry);
-    document.getElementById("Textinput").value="";
+
+    document.getElementById("Textinput").value = "";
 }
--- week8/trivia/index.js
+++ week8/trivia/index.js
@@ -1,20 +1,21 @@
-let correct = document.querySelector('.correct');
-correct.addEventListener('click', function() {
-    correct.style.backgroundColor = 'green';
+let correctAnswer = document.querySelector('.correct');
+correctAnswer.addEventListener('click', function() {
+    correctAnswer.style.backgroundColor = 'green';
     document.querySelector('#feedback1').innerHTML = 'Correct!';
 });
 
-let incorrects = document.querySelectorAll('.incorrect');
-for (let i = 0; i < incorrects.length; i++) {
-    incorrects[i].addEventListener('click', function() {
-        incorrects[i].style.backgroundColor = 'red';
+let incorrectAnswers = document.querySelectorAll('.incorrect');
+incorrectAnswers.forEach((incorrectAnswer) => {
+    incorrectAnswer.addEventListener('click', function() {
+        incorrectAnswer.style.backgroundColor = 'red';
         document.querySelector('#feedback1').innerHTML = 'incorrect';
     });
-}
+})
+
 
 document.querySelector('#check').addEventListener('click', function() {
     let input = document.querySelector('input');
-    if (input.value === 'Berlin' || 'berlin') {
+    if (input.value.toLowerCase() === 'berlin') {
         input.style.backgroundColor = 'green';
         document.querySelector('#feedback2').innerHTML = 'Correct';
     } else {
